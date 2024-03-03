a4280c4927c2adc89f71a9766e501e3a76d7d127
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-03-03 03:39:52 +0000

    feat(nvim): add lsp-powered symbol outline

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -99,6 +99,21 @@ return {
   },
 
   {
+    'hedyhli/outline.nvim',
+    lazy = true,
+    cmd = { 'Outline', 'OutlineOpen' },
+    keys = {
+      {
+        '<leader>o', '<cmd>Outline<CR>', desc = 'Toggle outline',
+      },
+    },
+    config = function()
+      require('outline').setup({
+      })
+    end,
+  },
+
+  {
     'lervag/vimtex',
     config = function()
       local opts = { noremap = true }
