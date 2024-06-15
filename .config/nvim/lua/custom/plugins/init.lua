532d599572612338aa166f2c9132f7f3a785a06d
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-06-15 23:11:13 +0100

    feat(nvim): revert noice dismiss keymap update

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -97,7 +97,7 @@ return {
         },
       }
       local opts = { noremap = true }
-      vim.keymap.set('n', '<leader>dn', '<cmd>NoiceDismiss<CR>', opts)
+      vim.keymap.set('n', '<leader>nd', '<cmd>NoiceDismiss<CR>', opts)
     end,
   },
 
