5d15182f0e5623e3ea44e18659e8afc41684b757
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-07-04 00:59:17 +0100

    feat: update jdtls

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,5 +1,5 @@
 local config = {
-  cmd = { '/opt/homebrew/Cellar/jdtls/1.36.0/bin/jdtls' },
+  cmd = { '/opt/homebrew/Cellar/jdtls/1.37.0/bin/jdtls' },
   root_dir = require('jdtls.setup').find_root { '.git' },
 }
 require('jdtls').start_or_attach(config)
