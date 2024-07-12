988401a880ca5cb63f49bad1468761decc0db74e
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-07-12 01:06:37 +0100

    feat(nvim): use a wildcard to find the jdtls executable

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,5 +1,21 @@
+-- Use a wildcard to find the jdtls executable
+local handle = io.popen 'ls /opt/homebrew/Cellar/jdtls/*/bin/jdtls 2>/dev/null | head -n 1'
+if not handle then
+  error 'Could not find jdtls executable'
+end
+
+local jdtls_path = handle:read '*a'
+handle:close()
+
+-- Remove trailing whitespace characters
+jdtls_path = jdtls_path:gsub('%s+', '')
+if jdtls_path == '' then
+  error 'Could not find jdtls executable'
+end
+
+-- Configure jdtls
 local config = {
-  cmd = { '/opt/homebrew/Cellar/jdtls/1.37.0/bin/jdtls' },
+  cmd = { jdtls_path },
   root_dir = require('jdtls.setup').find_root { '.git' },
 }
 require('jdtls').start_or_attach(config)
