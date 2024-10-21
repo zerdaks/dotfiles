530bc628ee242e22be428701898f1195384ecf4a
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-10-22 00:10:22 +0100

    feat: blur terminal

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,17 +1,19 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Mocha (Gogh)'
+config.color_scheme = "Catppuccin Mocha (Gogh)"
 
-config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 
-config.font = wezterm.font('Hack Nerd Font')
+config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 11.5
 
 config.hide_tab_bar_if_only_one_tab = true
 
 config.window_background_opacity = 0.7
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
+
+config.macos_window_background_blur = 10
 
 return config
