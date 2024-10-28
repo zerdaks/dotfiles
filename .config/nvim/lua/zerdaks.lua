bef96702ca68ee1d5f25e1a0f2e8a6faae30d237
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-10-28 01:58:47 +0000

    feat(nvim): update keymaps

--- .config/nvim/lua/zerdaks.lua
+++ .config/nvim/lua/zerdaks.lua
@@ -61,76 +61,60 @@ autocmd('BufWinEnter', {
 KEY MAPPINGS
 --]]
 
-local opts = { noremap = true }
+local keymap = vim.keymap
 
 -- NON-RECURSIVE NORMAL MODE
 
--- Go to the previous buffer
-vim.keymap.set('n', '<leader>,', '<cmd>bprevious<CR>', opts)
+-- Go to previous buffer
+keymap.set('n', '<leader>,', '<cmd>bprevious<CR>')
+
+-- Go to next buffer
+keymap.set('n', '<leader><leader>', '<cmd>bnext<CR>')
 
 -- Disable to avoid repeating previous commands by mistake
-vim.keymap.set('n', '<leader>.', '', opts)
-
--- Go to the next buffer
-vim.keymap.set('n', '<leader><leader>', '<cmd>bnext<CR>', opts)
+keymap.set('n', '<leader>.', '')
 
 -- Go to next diagnostic message
-vim.keymap.set('n', '<leader>d', vim.diagnostic.goto_next, opts)
+keymap.set('n', '<leader>d', vim.diagnostic.goto_next)
 
 -- Repeat latest f, t, F or T in opposite direction
-vim.keymap.set('n', "'", ',', opts)
+keymap.set('n', "'", ',')
 
 -- Copy relative file path to clipboard
-vim.keymap.set('n', '<leader>cp', '<cmd>let @* = expand("%:p:~:.")<CR>', opts)
+keymap.set('n', '<leader>cp', '<cmd>let @* = expand("%:p:~:.")<CR>')
 
--- Delete a buffer
-vim.keymap.set('n', '<leader>db', '<cmd>bd<CR>', opts)
+-- Delete current buffer
+keymap.set('n', '<leader>db', '<cmd>bd<CR>')
 
--- Save a view of the current buffer
-vim.keymap.set('n', '<leader>mk', '<cmd>mkview<CR>', opts)
+-- Save view of current buffer
+keymap.set('n', '<leader>mk', '<cmd>mkview<CR>')
 
--- Open a new tab page with an empty window
-vim.keymap.set('n', '<leader>to', '<cmd>tabedit<CR>', opts)
-
--- Center search results
-vim.keymap.set('n', 'n', 'nzz', opts)
-vim.keymap.set('n', 'N', 'Nzz', opts)
+-- Open new tab
+keymap.set('n', '<leader>to', '<cmd>tabedit<CR>')
 
 -- Center screen after scrolling
-vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
-vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
-
--- Disable movement with arrow keys
-vim.keymap.set('n', '<up>', '<nop>', opts)
-vim.keymap.set('n', '<down>', '<nop>', opts)
-vim.keymap.set('n', '<left>', '<nop>', opts)
-vim.keymap.set('n', '<right>', '<nop>', opts)
-
--- Disable movement with shift+arrow keys
-vim.keymap.set('n', '<S-up>', '<nop>', opts)
-vim.keymap.set('n', '<S-down>', '<nop>', opts)
-vim.keymap.set('n', '<S-left>', '<nop>', opts)
-vim.keymap.set('n', '<S-right>', '<nop>', opts)
+keymap.set('n', '<C-d>', '<C-d>zz')
+keymap.set('n', '<C-u>', '<C-u>zz')
 
 -- Navigate splits
-vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+keymap.set('n', '<C-h>', '<C-w>h')
+keymap.set('n', '<C-j>', '<C-w>j')
+keymap.set('n', '<C-k>', '<C-w>k')
+keymap.set('n', '<C-l>', '<C-w>l')
 
 -- Clear search highlights
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', opts)
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Do not copy deleted text
-vim.keymap.set('n', 'x', '"_x', opts)
+keymap.set('n', 'x', '"_x')
 
 -- NON-RECURSIVE VISUAL AND SELECT MODES
 
 -- Do not copy deleted text
-vim.keymap.set('v', 'd', '"_d', opts)
+keymap.set('v', 'd', '"_d')
 
 -- NON-RECURSIVE VISUAL MODE
 
 -- Paste the same value multiple times
 -- 'p' to put/paste, 'gv' to select the text that was put, and 'y' to copy the selected text
-vim.keymap.set('x', 'p', 'pgvy', opts)
+keymap.set('x', 'p', 'pgvy')
