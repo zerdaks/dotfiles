040fcf6ce42a4647bff57809bd370cff5b42c91d
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-11-03 01:48:55 +0000

    feat(nvim): rename plugin config files

--- .config/nvim/lua/custom/plugins/nvim-jdtls.lua
+++ .config/nvim/lua/custom/plugins/nvim-jdtls.lua
@@ -1,6 +1,7 @@
 return {
   {
     'mfussenegger/nvim-jdtls',
+    ft = 'java',
     config = function()
       vim.keymap.set('n', '<leader>i', '<cmd>lua require"jdtls".organize_imports()<CR>')
     end,
