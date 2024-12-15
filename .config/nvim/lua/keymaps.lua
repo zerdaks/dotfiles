31e12a4628942dfdccb6091d4c07bd1fdaf35f64
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-12-15 01:48:15 +0000

    feat(nvim): update keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,8 +10,8 @@ keymap.set('n', '<leader>,', '<cmd>bprevious<CR>')
 -- Go to next buffer
 keymap.set('n', '<leader><leader>', '<cmd>bnext<CR>')
 
--- Disable to avoid repeating previous commands by mistake
-keymap.set('n', '<leader>.', '')
+-- Go to previously opened buffer
+keymap.set('n', '<leader>.', '<cmd>b#<CR>')
 
 -- Go to next diagnostic message
 keymap.set('n', '<leader>d', vim.diagnostic.goto_next)
@@ -25,18 +25,14 @@ keymap.set('n', '<leader>cp', '<cmd>let @* = expand("%:p:~:.")<CR>')
 -- Delete current buffer
 keymap.set('n', '<leader>db', '<cmd>bd<CR>')
 
--- Go to previously opened buffer
-keymap.set('n', '<leader>.', '<cmd>b#<CR>')
-
 -- Save view of current buffer
 keymap.set('n', '<leader>mk', '<cmd>mkview<CR>')
 
 -- Open new tab
 keymap.set('n', '<leader>to', '<cmd>tabedit<CR>')
 
--- Center screen after scrolling
-keymap.set('n', '<C-d>', '<C-d>zz')
-keymap.set('n', '<C-u>', '<C-u>zz')
+-- Clear search highlights
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Disable movement with arrow keys
 vim.keymap.set('n', '<up>', '<nop>')
@@ -44,25 +40,24 @@ vim.keymap.set('n', '<down>', '<nop>')
 vim.keymap.set('n', '<left>', '<nop>')
 vim.keymap.set('n', '<right>', '<nop>')
 
--- Navigate splits
-keymap.set('n', '<C-h>', '<C-w>h')
-keymap.set('n', '<C-j>', '<C-w>j')
-keymap.set('n', '<C-k>', '<C-w>k')
-keymap.set('n', '<C-l>', '<C-w>l')
-
--- Clear search highlights
-keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Do not copy deleted text
-keymap.set('n', 'x', '"_x')
-
 -- VISUAL AND SELECT MODES
 
--- Do not copy deleted text
-keymap.set('v', 'd', '"_d')
+-- Disable movement with arrow keys
+vim.keymap.set('v', '<up>', '<nop>')
+vim.keymap.set('v', '<down>', '<nop>')
+vim.keymap.set('v', '<left>', '<nop>')
+vim.keymap.set('v', '<right>', '<nop>')
 
 -- VISUAL MODE
 
--- Paste the same value multiple times
+-- Paste the same value over selected text multiple times
 -- 'p' to put/paste, 'gv' to select the text that was put, and 'y' to copy the selected text
 keymap.set('x', 'p', 'pgvy')
+
+-- INSERT MODE
+
+-- Disable movement with arrow keys
+vim.keymap.set('i', '<up>', '<nop>')
+vim.keymap.set('i', '<down>', '<nop>')
+vim.keymap.set('i', '<left>', '<nop>')
+vim.keymap.set('i', '<right>', '<nop>')
