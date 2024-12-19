04b745b7877d8c56046ee967dacc5ba2e3b476ab
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-12-19 01:00:18 +0000

    feat(nvim): update oil icons

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -1,13 +1,18 @@
 return {
   {
     'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
     opts = {
       view_options = {
         show_hidden = true,
       },
     },
     dependencies = {
-      'nvim-tree/nvim-web-devicons',
+      {
+        'echasnovski/mini.icons',
+        opts = {},
+      },
     },
     vim.keymap.set('n', '-', '<cmd>Oil<CR>'),
   },
