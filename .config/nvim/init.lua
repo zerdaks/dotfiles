ffe47500219922367acc274a03f3259bbb9797c9
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-04-16 10:06:44 +0100

    remove lsp support for typescript

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -689,8 +689,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
-        eslint = {},
+        -- ts_ls = {},
         --
 
         lua_ls = {
