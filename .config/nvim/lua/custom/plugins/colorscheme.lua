ea6c7fe197490168147a1f9822ffad87e6c1705f
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-04-16 10:50:21 +0100

    feat(nvim): update kickstart, use tokyo night

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'catppuccin'
+      -- vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
@@ -21,7 +21,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 }
