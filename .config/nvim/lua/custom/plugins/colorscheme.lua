501e7fa13cbfa81624809f26942f59448781a02c
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-04-18 18:59:26 +0100

    feat(nvim): install github theme

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -17,11 +17,21 @@ return {
   },
 
   {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'github_dark_colorblind'
+    end,
+  },
+
+  {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 }
