efb1331fc0a622bf8938f1f88bc49a2e8f210415
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-05-10 21:46:21 +0100

    fix(nvim): enable completion for obsidian nvim

--- .config/nvim/lua/custom/plugins/obsidian.lua
+++ .config/nvim/lua/custom/plugins/obsidian.lua
@@ -5,6 +5,23 @@ return {
   ft = 'markdown',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    {
+      'hrsh7th/nvim-cmp',
+      config = function()
+        local cmp = require 'cmp'
+        cmp.setup {
+          mapping = {
+            ['<Down>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+            ['<Up>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+            ['<C-y>'] = cmp.mapping.confirm { select = true },
+          },
+          sources = {
+            { name = 'nvim_lsp' },
+            { name = 'buffer' },
+          },
+        }
+      end,
+    },
   },
   opts = {
     workspaces = {
