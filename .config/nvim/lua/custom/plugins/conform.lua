ad9f6ad94a0b51e4502fdafebb380aa2f5da25c1
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-06-03 13:33:28 +0100

    feat(nvim): use gofmt and goimports for Go formatting

--- .config/nvim/lua/custom/plugins/conform.lua
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -2,6 +2,7 @@ return {
   'stevearc/conform.nvim',
   opts = {
     formatters_by_ft = {
+      go = { 'gofmt', 'goimports' },
       html = { 'prettier' },
       javascript = { 'prettier' },
       lua = { 'stylua' },
