46d2e3b404ae304c024d135d99e68d5e60c68429
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-07-15 10:47:10 +0100

    feat(fish): add alias to delete kubernetes jobs

--- .config/fish/config-local.fish
+++ .config/fish/config-local.fish
@@ -9,22 +9,28 @@ alias awsprofile 'export AWS_PROFILE=$(aws configure list-profiles | fzf)'
 alias ns 'kubens | fzf | xargs -I {} kubens {}'
 alias ctx 'kubectx | fzf | xargs -I {} kubectx {}'
 
-alias ctldelete 'kubectl delete pod $(__fzf_pods)'
+alias ctldeljobs 'kubectl delete job $(__fzf_jobs)'
+alias ctldelpods 'kubectl delete pod $(__fzf_pods)'
 alias ctldeployments 'kubectl get deployments'
 alias ctldescribe 'kubectl describe pod $(__fzf_pods)'
-alias ctllogs '__ctllogs'
+alias ctllogs '__ctl_logs'
 alias ctlevents 'kubectl get events --field-selector involvedObject.name=$(__fzf_pods)'
 alias ctlnamespace 'kubectl config view --minify -o jsonpath="{.contexts[0].context.namespace}"'
+alias ctljobs 'kubectl exec -it $(__fzf_jobs) -- /bin/sh'
 alias ctlpods 'kubectl exec -it $(__fzf_pods) -- /bin/sh'
 alias ctlsecrets 'kubectl get secret $(__fzf_secrets) -o json | jq -r ".data | to_entries[] | \"\(.key): \(.value | @base64d)\""'
 
-function __ctllogs
+function __ctl_logs
     set pod (__fzf_pod)
     kubectl logs $pod >> "$pod.log"
 end
 
 function __fzf_pod
     kubectl get pods | fzf --bind 'ctrl-r:reload(kubectl get pods)' --header "Press 'ctrl-r' to reload" | awk '{print $1}'
+end
+
+function __fzf_jobs
+    kubectl get jobs | fzf --multi --bind 'ctrl-r:reload(kubectl get jobs)' --header "Press 'ctrl-r' to reload" | awk '{print $1}'
 end
 
 function __fzf_pods
