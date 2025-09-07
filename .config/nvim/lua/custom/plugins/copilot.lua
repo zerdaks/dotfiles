9c8998cee6400f2440d96f8f0672d869e31b9768
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-09-07 15:17:46 +0100

    feat(nvim): add CopilotChat.nvim plugin integration

--- .config/nvim/lua/custom/plugins/copilot.lua
+++ .config/nvim/lua/custom/plugins/copilot.lua
@@ -1,11 +1,23 @@
 return {
-  'github/copilot.vim',
-  config = function()
-    vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept("")', {
-      expr = true,
-      silent = true,
-      noremap = true,
-    })
-    vim.g.copilot_no_tab_map = true
-  end,
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept("")', {
+        expr = true,
+        silent = true,
+        noremap = true,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {},
+    vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChatCommit<CR>'),
+  },
 }
