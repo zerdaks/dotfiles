08e227ab27950d17338803f5f39ffd81436b3758
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-09-10 09:28:23 +0100

    feat(nvim): set two-space indent for Prisma files

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -28,6 +28,17 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+-- Configure indentation for Prisma files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'prisma',
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
+
 -- Configure white space
 vim.opt.list = true
 vim.opt.listchars = {
