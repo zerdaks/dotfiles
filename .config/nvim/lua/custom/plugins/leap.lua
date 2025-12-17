493be049ef8a3fec7b6c780c44537e70f264adb6
zerdaks <7451424+zerdaks@users.noreply.github.com>
2025-12-17 10:52:12 +0000

    chore(nvim): restore leap plugin

---
+++ .config/nvim/lua/custom/plugins/leap.lua
@@ -0,0 +1,7 @@
+return {
+  'ggandor/leap.nvim',
+  config = function()
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+  end,
+}
