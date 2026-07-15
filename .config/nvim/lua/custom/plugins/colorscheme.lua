95be3ef48abc07fe51ef846e5b29c6b834e7f37b
zerdaks <7451424+zerdaks@users.noreply.github.com>
2026-07-15 05:22:17 +0100

    chore(nvim): comment out unused colorscheme plugins

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -8,20 +8,20 @@ return {
     end,
   },
 
-  {
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000,
-    config = function()
-      -- vim.cmd.colorscheme 'gruvbox'
-    end,
-  },
+  -- {
+  --   'ellisonleao/gruvbox.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'gruvbox'
+  --   end,
+  -- },
 
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --   end,
+  -- },
 }
