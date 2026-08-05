3c20ab2d019a98fe22b8b7cf42cc03fded7957f4
zerdaks <7451424+zerdaks@users.noreply.github.com>
2026-08-05 18:55:57 +0100

    fix(nvim): unpin orgmode from 0.7.0
    org-roam declared orgmode as a dependency with tag 0.7.0, which lazy.nvim merged into the top-level spec, holding orgmode 11 months stale.

--- .config/nvim/lua/custom/plugins/orgmode.lua
+++ .config/nvim/lua/custom/plugins/orgmode.lua
@@ -20,10 +20,7 @@ return {
     'chipsenkbeil/org-roam.nvim',
     tag = '0.2.0',
     dependencies = {
-      {
-        'nvim-orgmode/orgmode',
-        tag = '0.7.0',
-      },
+      'nvim-orgmode/orgmode',
     },
     config = function()
       require('org-roam').setup {
